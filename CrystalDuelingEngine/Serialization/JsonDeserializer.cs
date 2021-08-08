7f39c2e9da6ff8205eb2d8f4728e784202eb2aac
SaberSnail <andrew@batishko.net>
2021-08-08 07:48:14 -0700

    Add default JSON serialization

---
+++ CrystalDuelingEngine/Serialization/JsonDeserializer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+
+namespace CrystalDuelingEngine.Serialization
+{
+	public class JsonDeserializer : IDeserializer
+	{
+		public static ISerializable Deserialize(string text)
+		{
+			using var document = JsonDocument.Parse(text);
+
+			var deserializer = new JsonDeserializer(document.RootElement);
+			return SerializationManager.Deserialize(deserializer);
+		}
+
+		public T GetValue<T>(string key)
+		{
+			if (!m_root.TryGetProperty(key, out var property))
+				return default(T);
+			return GetOrDeserializeValue<T>(property);
+		}
+
+		public IEnumerable<T> GetValues<T>(string key)
+		{
+			if (!m_root.TryGetProperty(key, out var property) || property.ValueKind == JsonValueKind.Null)
+				return default(IEnumerable<T>);
+			return property.EnumerateArray().Select(GetOrDeserializeValue<T>);
+		}
+
+		private JsonDeserializer(JsonElement root) => m_root = root;
+
+		private T GetOrDeserializeValue<T>(JsonElement property)
+		{
+			if (property.ValueKind == JsonValueKind.Object)
+			{
+				var childDeserializer = new JsonDeserializer(property);
+				return (T) SerializationManager.Deserialize(childDeserializer);
+			}
+			if (property.ValueKind == JsonValueKind.Array)
+			{
+				var argType = typeof(T).GetGenericArguments()[0];
+				var getOrDeserializeValue = GetType().GetMethod(nameof(GetOrDeserializeValue), BindingFlags.NonPublic | BindingFlags.Instance).MakeGenericMethod(argType);
+				var cast = typeof(Enumerable).GetMethod("Cast", BindingFlags.Public | BindingFlags.Static).MakeGenericMethod(argType);
+				var toList = typeof(Enumerable).GetMethod("ToList", BindingFlags.Public | BindingFlags.Static).MakeGenericMethod(argType);
+				return (T) toList.Invoke(null, new[] { cast.Invoke(null, new[] { property.EnumerateArray().Select(x => getOrDeserializeValue.Invoke(this, new object[] { x })) }) });
+			}
+			if (typeof(T).IsEnum)
+				return (T) Enum.Parse(typeof(T), property.GetString());
+
+			return System.Text.Json.JsonSerializer.Deserialize<T>(property.GetRawText());
+		}
+
+		JsonElement m_root;
+	}
+}
---
+++ CrystalDuelingEngine/Serialization/JsonSerializer.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace CrystalDuelingEngine.Serialization
+{
+	public class JsonSerializer : ISerializer
+	{
+		public static string Serialize(ISerializable serializable)
+		{
+			var options = new JsonWriterOptions
+			{
+				Indented = true,
+				Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+			};
+
+			using var stream = new MemoryStream();
+			var writer = new Utf8JsonWriter(stream, options);
+
+			var serializer = new JsonSerializer(writer);
+			serializable.Serialize(serializer);
+
+			writer.Flush();
+			return Encoding.UTF8.GetString(stream.ToArray());
+		}
+
+		public void StartObject(ISerializable obj)
+		{
+			var isSameAsPreviousObject = IsObjectSameAsPreviousObject(obj);
+			m_objectStack.Push(obj);
+			if (!isSameAsPreviousObject)
+			{
+				if (m_nestedKey is null)
+				{
+					m_writer.WriteStartObject();
+				}
+				else
+				{
+					m_writer.WriteStartObject(m_nestedKey);
+					m_nestedKey = null;
+				}
+				SerializationManager.StartObjectSerialization(this, obj);
+			}
+		}
+
+		public void EndObject()
+		{
+			var obj = m_objectStack.Pop();
+			if (!IsObjectSameAsPreviousObject(obj))
+				m_writer.WriteEndObject();
+		}
+
+		private JsonSerializer(Utf8JsonWriter writer)
+		{
+			m_objectStack = new Stack<ISerializable>();
+			m_writer = writer;
+		}
+
+		private bool IsObjectSameAsPreviousObject(ISerializable obj)
+		{
+			return m_objectStack.Count != 0 && ReferenceEquals(obj, m_objectStack.Peek());
+		}
+
+		public void SerializeValue(string key, object value)
+		{
+			var type = value?.GetType();
+			if (value is null)
+			{
+				m_writer.WriteNull(key);
+			}
+			else if (value is ISerializable serializable)
+			{
+				m_nestedKey = key;
+				serializable.Serialize(this);
+			}
+			else if (type.IsEnum)
+			{
+				m_writer.WriteString(key, value.ToString());
+			}
+			else if (value is bool boolValue)
+			{
+				m_writer.WriteBoolean(key, boolValue);
+			}
+			else if (value is DateTime dateTimeValue)
+			{
+				m_writer.WriteString(key, dateTimeValue);
+			}
+			else if (value is DateTimeOffset dateTimeOffsetValue)
+			{
+				m_writer.WriteString(key, dateTimeOffsetValue);
+			}
+			else if (value is Guid guidValue)
+			{
+				m_writer.WriteString(key, guidValue);
+			}
+			else if (value is string stringValue)
+			{
+				m_writer.WriteString(key, stringValue);
+			}
+			else if (value is IEnumerable enumerable)
+			{
+				m_writer.WriteStartArray(key);
+				foreach (var singleValue in enumerable)
+					SerializeValueInArray(singleValue);
+				m_writer.WriteEndArray();
+			}
+			else
+			{
+				if (!WriteNumber(key, value))
+					throw new SerializationFormatException($"Serialized object must be IEnumerable, ISerializable, or simple: {value.GetType().FullName}");
+			}
+		}
+
+		private bool WriteNumber(string key, object value)
+		{
+			var writerType = m_writer.GetType();
+			var argType = value.GetType();
+			var numberMethod = writerType.GetMethod("WriteNumber", new[] { typeof(string), argType });
+			if (numberMethod is not null)
+			{
+				numberMethod.Invoke(m_writer, new[] { key, value });
+				return true;
+			}
+			return false;
+		}
+
+		private void SerializeValueInArray(object value)
+		{
+			var type = value?.GetType();
+			if (value is null)
+			{
+				m_writer.WriteNullValue();
+			}
+			else if (value is ISerializable serializable)
+			{
+				m_nestedKey = null;
+				serializable.Serialize(this);
+			}
+			else if (type.IsEnum)
+			{
+				m_writer.WriteStringValue(value.ToString());
+			}
+			else if (value is bool boolValue)
+			{
+				m_writer.WriteBooleanValue(boolValue);
+			}
+			else if (value is DateTime dateTimeValue)
+			{
+				m_writer.WriteStringValue(dateTimeValue);
+			}
+			else if (value is DateTimeOffset dateTimeOffsetValue)
+			{
+				m_writer.WriteStringValue(dateTimeOffsetValue);
+			}
+			else if (value is Guid guidValue)
+			{
+				m_writer.WriteStringValue(guidValue);
+			}
+			else if (value is string stringValue)
+			{
+				m_writer.WriteStringValue(stringValue);
+			}
+			else if (value is IEnumerable enumerable)
+			{
+				m_writer.WriteStartArray();
+				foreach (var singleValue in enumerable)
+					SerializeValueInArray(singleValue);
+				m_writer.WriteEndArray();
+			}
+			else
+			{
+				if (!WriteNumberValue(value))
+					throw new SerializationFormatException($"Serialized object must be IEnumerable, ISerializable, or simple: {value.GetType().FullName}");
+			}
+		}
+
+		private bool WriteNumberValue(object value)
+		{
+			var writerType = m_writer.GetType();
+			var argType = value.GetType();
+			var numberMethod = writerType.GetMethod("WriteNumberValue", new[] { argType });
+			if (numberMethod is not null)
+			{
+				numberMethod.Invoke(m_writer, new[] { value });
+				return true;
+			}
+			return false;
+		}
+
+		string m_nestedKey;
+		Stack<ISerializable> m_objectStack;
+		Utf8JsonWriter m_writer;
+	}
+}
